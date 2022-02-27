25ce50c532791b2695d12015d570ad806614ba65
ivanfestina2 <ivanfestina@gmail.com>
2022-02-27 18:41:29 +0300

    almost ended basic structure

---
+++ src/components/footer/footerNav/FooterNav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './FooterNav.module.css'
+
+export const FooterNav = () => {
+    return (
+        <div className={s.footerNav}>
+            <div><a href={''}></a></div>
+            <div><a href={''}></a></div>
+            <div><a href={''}></a></div>
+            <div><a href={''}></a></div>
+            <div><a href={''}></a></div>
+        </div>
+    )
+}
