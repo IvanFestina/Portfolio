fafe0589b4a027931f001b4c6a39dbbecc4c7675
IvanFestina <ivanfestina@gmail.com>
2022-04-03 23:57:56 +0300

    in progress...

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -8,18 +8,12 @@ export const Contacts = () => {
         <div className={s.contactsBlock}>
             <div className={`${style.container} ${s.container}`}>
                 <h2>Contacts</h2>
-                <div className={s.form}>
-                    <form action="/my-handling-form-page" method="post">
-
-                        <input type="text" id="name" name="user_name"/>
-
-                        <input type="text" id="mail" name="user_mail"/>
-
-                        <textarea id="msg" name="user_message"></textarea>
-
-                    </form>
-                </div>
-                <button>Sent</button>
+                <form className={s.form}>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <textarea id="msg" name="user_message"></textarea>
+                    <button type='submit' onClick={(event) => {event.preventDefault()}}>Sent</button>
+                </form>
             </div>
         </div>
     )
