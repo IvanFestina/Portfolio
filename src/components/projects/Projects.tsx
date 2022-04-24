3d09d8b1f7b5b7d9e9f4f29e4544ca1ef77b1f6e
IvanFestina <ivanfestina@gmail.com>
2022-04-24 23:41:29 +0300

    in progress...

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import s from './Projects.module.css'
 import styleContainer from '../../common/styles/Container.module.css'
 import {Project} from "./project/Project";
+import {Title} from "../../common/components/Title";
 
 
 export const Projects = () => {
+    // const socialNetwork = {
+    //     backgroundImage: 'url(' + imgUrl + ')',
+    // }
     return (
         <div className={s.projectBlock}>
             <div className={`${styleContainer.container} ${s.projectContainer}`}>
-                <div  className={s.title}>
-                    <h2>My projects</h2>
-                </div>
+                <Title text={'Projects'}/>
                 <div className={s.projects}>
                     <Project title={'Social Network Project'}
                              description={'This is a model of basic social network to assess my skill'}/>
                     <Project title={'TodoList'}
                              description={'Here you can find interactive todolist for your needs'}/>
+                    <Project title={'Memorize'}
+                             description={'Team project for learning and memorizing with use of cards'}/>
                 </div>
             </div>
         </div>
