a7db7a8d1c4b43621cc53c216c10d88f7ee802f0
IvanFestina <ivanfestina@gmail.com>
2022-04-26 23:56:37 +0300

    in progress...

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import s from './Main.module.css'
+import s from './Main.module.scss'
 import styleContainer from './../../common/styles/Container.module.css'
-
+import MainPhoto from '../../assets/image/MainPhoto.jpg'
 export const Main = () => {
+
+ const Main_Photo = {
+        backgroundImage: `url( ${MainPhoto} )`
+    }
+
     return (
         <div className={s.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={s.greetings}>
-                    <span>Hello!</span>
-                    <h1>I am Ivan Andreev</h1>
-                    <span>Front-End Developer</span>
+                    <span>Hi there!</span>
+                    <h1>I'm Ivan</h1>
+                    <span>And I'm a Front-End Developer</span>
                 </div>
-                <div className={s.photo}>Photo will be placed here</div>
+                <div className={s.photo} style={Main_Photo}/>
             </div>
         </div>
     )
--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,21 +1,36 @@
 import React from "react";
-import s from './Skills.module.css'
+import s from './Skills.module.scss'
 import styleContainer from '../../common/styles/Container.module.css'
 import {Skill} from "./skill/skill";
 import {Title} from "../../common/components/Title";
+import JSTS from "../../assets/image/TSpng.png";
+import HTMLCSS from "../../assets/image/HTMLCSS.png";
+import ReactLogo from "../../assets/image/React.png";
 
 
 export const Skills = () => {
+
+
+    const JS_TS = {
+        backgroundImage: `url(${JSTS})`
+    }
+    const HTML_CSS = {
+        backgroundImage: `url(${HTMLCSS})`
+    }
+    const React_Logo = {
+        backgroundImage: `url(${ReactLogo})`
+    }
+
     return (
         <div className={s.skillBlock}>
             <div className={`${styleContainer.container} ${s.skillContainer}`}>
                 <Title text={'Skills'}/>
                 <div className={s.skills}>
-                    <Skill title={'HTML CSS'}
+                    <Skill title={'HTML CSS'} style={HTML_CSS}
                            description={'Using my knowledge to make UI more interactive and adaptable'}/>
-                    <Skill title={'JS TS'}
+                    <Skill title={'TypeScript'} style={JS_TS}
                            description={'Applying JavaScript with TypeScript to make apps more stable'}/>
-                    <Skill title={'React'} description={'Using this library to make magic happen'}/>
+                    <Skill title={'React'} style={React_Logo} description={'Using this library to make magic happen'}/>
                 </div>
             </div>
         </div>
