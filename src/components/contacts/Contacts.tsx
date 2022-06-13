5d483ec2306cac7d6c0bb77bb54c9315ff7a93f7
IvanFestina <ivanfestina@gmail.com>
2022-06-13 23:08:29 +0300

    in progress...

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,20 +1,116 @@
-import React from 'react'
+import React, {useState} from 'react'
 import s from './Contacts.module.scss'
 import style from '../../common/styles/Container.module.css'
+import {Title} from "../../common/components/Title";
+import {useFormik} from "formik";
+import TextField from "@material-ui/core/TextField/TextField";
+import {styled} from "@material-ui/core";
+
+type FormikErrorType = {
+    name?: string
+    email?: string
+    message?: string
+}
 
 export const Contacts = () => {
 
+    const [message, setMessage] = useState<string>('The message was sent')
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: '',
+        },
+        validate: values => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.name) {
+                errors.name = 'Required';
+            } else if (values.name.length > 20) {
+                errors.name = 'Must be 20 characters or less';
+            }
+            if (!values.message) {
+                errors.message = 'Required';
+            } else if (values.message.length > 2000) {
+                errors.message = 'Must be 2000 characters or less';
+            }
+            return errors;
+        },
+        onSubmit: values => {
+
+        }
+    })
+
     return (
-        <div className={s.contactsBlock}>
-            <div className={`${style.container} ${s.container}`}>
-                <h2 className={s.title}>Contacts</h2>
-                <form className={s.form}>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <textarea id="msg" name="user_message"></textarea>
-                    <button type='submit' onClick={(event) => {event.preventDefault()}}>Sent</button>
-                </form>
+        <div id='contacts' className={s.contactsBlock}>
+            <div className={`${style.container} ${s.contactsContainer}`}>
+                <Title text={'Contacts'} style={{color: '#294861'}}/>
+                <div className={s.form}>
+                    <CssTextField className={s.mUIInput}
+                                  InputLabelProps={{className: s.label}}
+                                  id="custom-css-outlined-input"
+                                  label="Name"
+                                  margin="normal"
+                                  {...formik.getFieldProps('name')}/>
+                    {
+                        formik.touched.name
+                        && formik.errors.name
+                        && <div
+                            style={{color: '#294861'}}>{formik.errors.name}</div>
+                    }
+
+                    <CssTextField className={s.mUIInput}
+                                  InputLabelProps={{className: s.label}}
+                                  id="custom-css-outlined-input"
+                                  label="Email"
+                                  margin="normal"
+                                  {...formik.getFieldProps('email')}/>
+                    {formik.touched.email
+                    && formik.errors.email
+                    &&
+                    <div style={{color: '#294861'}}>{formik.errors.name}</div>
+                    }
+
+                    <CssTextField className={s.mUITextarea}
+                                  InputLabelProps={{className: s.label}}
+                                  id="custom-css-outlined-input"
+                                  multiline
+                                  minRows={3}
+                                  label="Message" margin="normal"
+                                  {...formik.getFieldProps('message')}/>
+                    {
+                        formik.touched.message
+                        && formik.errors.message
+                        && <div
+                            style={{color: '#294861'}}>{formik.errors.message}</div>}
+
+                    <div className={s.terms}>
+                        <button className={s.button}>Send</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
+
+
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+        color: '#DEB112',
+    },
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: '#294861',
+        },
+        '&:hover fieldset': {
+            borderColor: '#DEB112',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#DEB112',
+        },
+    },
+});
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from './Footer.module.scss'
 import style from '../../common/styles/Container.module.css'
-import {FooterNav} from "./footerNav/FooterNav";
+import {FaGithubSquare, FaLinkedin, FaTelegram} from "react-icons/fa";
 
 export const Footer = () => {
     return (
@@ -9,13 +9,15 @@ export const Footer = () => {
             <div className={`${style.container} ${s.container}`}>
                 <h2 className={s.title}>Ivan Andreev</h2>
                 <div className={s.socialIcons}>
-                    <div className={s.socialIcon}><a href={''}></a></div>
-                    <div className={s.socialIcon}><a href={''}></a></div>
-                    <div className={s.socialIcon}><a href={''}></a></div>
+                    <div className={s.socialIcon}><FaLinkedin color='#294861'
+                                                              size={'3rem'}/></div>
+                    <div className={s.socialIcon}><FaTelegram color='#294861'
+                                                              size={'3rem'}/></div>
+                    <div className={s.socialIcon}><FaGithubSquare color='#294861'
+                                                                  size={'3rem'}/></div>
                 </div>
-                <h3>2022 All rights reserved</h3>
+                <h3 className={s.rights}>2022 All rights reserved</h3>
             </div>
-
         </div>
     )
 }
