05b3f4406431cc15da23bc98d015368827de40ce
IvanFestina <ivanfestina@gmail.com>
2022-06-13 23:08:29 +0300

    in progress...

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -9,8 +9,10 @@ import {
     SiRedux,
     SiHtml5,
     SiTypescript,
-    SiReact
+    SiReact, SiMaterialui, SiStorybook, SiJest
 } from 'react-icons/si'
+import {FaExchangeAlt, FaWpforms} from "react-icons/fa/";
+import {FaGithubSquare} from "react-icons/fa";
 
 
 export const Skills = () => {
@@ -21,14 +23,27 @@ export const Skills = () => {
             <div className={`${styleContainer.container} ${s.skillContainer}`}>
                 <Title text={'Skills'} style={{color: '#294861'}}/>
                 <div className={s.skills}>
-                    <Skill title={'HTML'} icon={<SiHtml5 color='#294861' size={'4rem'}/>}
+                    <Skill title={'JavaScript'}
+                           icon={<SiJavascript color='#FFFFE0' size={'4rem'}/>}/>
+                    <Skill title={'TypeScript'}
+                           icon={<SiTypescript color=' #FFFFE0' size={'4rem'}/>}/>
+                    <Skill title={'React'}
+                           icon={<SiReact color=' #FFFFE0' size={'4rem'}/>}
+                           description={'Using this library to make magic happen'}/>
+                    <Skill title={'Redux/Thunk/Toolkit'}
+                           icon={<SiRedux color=' #FFFFE0' size={'4rem'}/>}/>
+                    <Skill title={'Formik / React-Hook-Form'}
+                           icon={<FaWpforms color=' #FFFFE0' size={'4rem'}/>}/>
+                    <Skill title={'MaterialUI / AntDesign'}
+                           icon={<SiMaterialui color=' #FFFFE0' size={'4rem'}/>}/>
+                    <Skill title={'Git / GitHub'}
+                           icon={<FaGithubSquare color=' #FFFFE0' size={'4rem'}/>}/>
+                    <Skill title={'Storybook'}
+                           icon={<SiStorybook color=' #FFFFE0' size={'4rem'}/>}/>
+                    <Skill title={'TDD, Jest'}
+                           icon={<SiJest color=' #FFFFE0' size={'4rem'}/>}/>
+                    <Skill title={'Axios'} icon={<FaExchangeAlt color='#FFFFE0' size={'4rem'}/>}
                            description={'Using my knowledge to make UI more interactive and adaptable'}/>
-                    <Skill title={'CSS/SCSS'} icon={<SiCss3 color='#FFFFE0' size={'4rem'}/>}/>
-                    <Skill title={'JavaScript'} icon={<SiJavascript color='#FFFFE0' size={'4rem'}/>}/>
-                    <Skill title={'TypeScript'} icon={<SiTypescript color=' #FFFFE0' size={'4rem'}/>}/>
-                    <Skill title={'React'} icon={<SiReact color=' #FFFFE0' size={'4rem'}/>} description={'Using this library to make magic happen'}/>
-                    <Skill title={'Redux/Toolkit'} icon={<SiRedux color=' #FFFFE0' size={'4rem'}/>}/>
-                    <Skill title={'Redux Toolkit'} icon={<SiJavascript color=' #FFFFE0' size={'4rem'}/>}/>
                 </div>
             </div>
         </div>
