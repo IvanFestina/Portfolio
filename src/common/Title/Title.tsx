c5f00f177b9a02388c52ced0f3b7eab744514530
IvanFestina <ivanfestina@gmail.com>
2022-06-17 16:16:46 +0300

    framer motion

--- src/common/Title/Title.tsx
+++ src/common/Title/Title.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import s from './Title.module.scss'
+import {motion} from "framer-motion";
+
 
 type TitlePropsType = { text: string, style?: object }
 
+const titleAnimation = {
+    hidden: {
+        y: 100,
+        opacity: 0,
+    },
+    visible: {
+        y: 0,
+        opacity: 1,
+    },
+}
+
 export const Title = ({text, style}: TitlePropsType) => {
     return (
-        <div className={s.title} style={style}>
-            <h2>{text}</h2>
-        </div>
+        <motion.div className={s.title} initial='hidden'
+                    whileInView='visible' variants={titleAnimation}
+                    transition={{delay: 0.4, duration: 0.5}} style={style}>
+            <motion.h2 variants={titleAnimation}
+                       transition={{delay: 0.2, duration: 0.5}}>{text}</motion.h2>
+        </motion.div>
     )
 }
