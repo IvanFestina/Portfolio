de09d8a05bd7ee0415558a8ba092b70d0ffa1160
IvanFestina <ivanfestina@gmail.com>
2022-07-29 22:15:10 +0300

    Added one project

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Projects.module.scss'
 import {Project} from './project/Project';
 import {Title} from '../../common/Title/Title';
 import DemoTodolistProject from './../../assets/image/DemoTodolistProject.jpg'
 import DemoSocialNetworkProject from './../../assets/image/DemoSocialNetworkProject.png'
 import DemoFlashCardsProject from './../../assets/image/DemoFlashCardsProject.jpg'
+import DemoTaxiApp from './../../assets/image/Taxi_app.jpg'
+import Modal from "../../common/components/Modal";
 
 export const Projects = () => {
+
+    const [isModal, setIsModal] = useState<boolean>(false)
+
 
     const socialNetwork = {
         backgroundImage: `url(${DemoSocialNetworkProject})`
@@ -16,6 +21,9 @@ export const Projects = () => {
     }
     const flashCards = {
         backgroundImage: `url( ${DemoFlashCardsProject} )`,
+    }
+    const taxiApp = {
+        backgroundImage: `url( ${DemoTaxiApp})`
     }
 
 
@@ -27,28 +35,35 @@ export const Projects = () => {
                     <Project title={'Memorize'}
                              description={'Team project for learning and memorizing with use of cards'}
                              style={flashCards}
-                             urlDemo={'https://github.com/msseleznev/friday-app'}
+                             urlDemo={'https://msseleznev.github.io/friday-app'}
                              urlCode={'https://github.com/msseleznev/friday-app'}
-                             />
+                    />
                     <Project title={'TodoList'}
                              description={'Here you can find interactive todolist for your needs'}
                              style={todolist}
-                             urlDemo={'https://todolist-toolkit-ts-react.herokuapp.com'}
+                             urlDemo={'https://ivanfestina.github.io/todolist-project'}
                              urlCode={'https://github.com/IvanFestina/todolist-project'}
                     />
                     <Project title={'Social Network Project'}
                              description={'This is a model of basic social network to assess my skill'}
                              style={socialNetwork}
-                             urlDemo={''}
-                             urlCode={''}
+                             urlDemo={'https://ivanfestina.github.io/TS-React-Social-NetWork'}
+                             urlCode={'https://github.com/IvanFestina/TS-React-Social-NetWork'}
                     />
                     <Project title={'Taxi - React Native App'}
                              description={'Frontend part of a mobile app for ordering a taxi, picking up a destination and more'}
-                             style={''}
-                             urlDemo={''}
-                             urlCode={''}
+                             style={taxiApp}
+                             urlCode={'https://github.com/IvanFestina/Taxi-react-native-app'}
+                             setCallback={setIsModal}
                     />
                 </div>
+                <Modal active={isModal} setActive={setIsModal}>
+                    <iframe width="600" height="900"
+                            src="https://www.youtube.com/embed/q-1LX_JUW3w"
+                            title="Taxi react-native app (demo)" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen/>
+                    </Modal>
             </div>
         </div>
     )
--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import s from './Project.module.scss'
 import {motion} from "framer-motion"
-import {Button, ButtonGroup, Link} from "@material-ui/core";
 
 
 export type ProjectProps = {
@@ -10,20 +9,38 @@ export type ProjectProps = {
     style: any
     urlDemo?: string
     urlCode?: string
+    setCallback?: (toggle: boolean) => void
 }
 
-export const Project = ({description, title, style, urlDemo, urlCode, ...props}: ProjectProps) => {
+export const Project = ({
+                            description,
+                            title,
+                            style,
+                            urlDemo,
+                            urlCode,
+                            setCallback
+                        }: ProjectProps) => {
     return (
         <div className={s.project}>
             <motion.div
                 whileHover={{scale: 1.03, transition: {ease: "easeOut", duration: 0.5}}}
                 className={s.projectImage} style={style}>
-                <a href={urlDemo}>
-                    <motion.button whileTap={{scale: 1}} whileHover={{scale: 1.2}}
-                                   transition={{type: "spring", stiffness: 300}}
-                                   className={s.viewBtn}>Demo
-                    </motion.button>
-                </a>
+                {
+                    setCallback ?
+                        <motion.button
+                            onClick={() => setCallback(true)}
+                            whileTap={{scale: 1}} whileHover={{scale: 1.2}}
+                            transition={{type: "spring", stiffness: 300}}
+                            className={s.viewBtn}>Video
+                        </motion.button>
+                        :
+                        <a href={urlDemo}>
+                            <motion.button whileTap={{scale: 1}} whileHover={{scale: 1.2}}
+                                           transition={{type: "spring", stiffness: 300}}
+                                           className={s.viewBtn}>Demo
+                            </motion.button>
+                        </a>
+                }
                 <a href={urlCode}>
                     <motion.button whileTap={{scale: 1}} whileHover={{scale: 1.2}}
                                    transition={{type: "spring", stiffness: 300}}
